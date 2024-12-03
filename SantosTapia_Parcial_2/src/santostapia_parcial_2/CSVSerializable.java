d3818da9afd37b4ec9dc24ab11346a7b5e9a704b
BautistaSantosTapia <113394255+BautistaSantosTapia@users.noreply.github.com>
2024-12-02 22:19:35 -0300

    Delete SantosTapia_Parcial_2 directory

--- SantosTapia_Parcial_2/src/santostapia_parcial_2/CSVSerializable.java
+++
@@ -1,9 +0,0 @@
-
-package santostapia_parcial_2;
-
-public interface CSVSerializable {
-    
-    String toCSV();
-    
-    //String toHeaderCSV();
-}
--- SantosTapia_Parcial_2/src/santostapia_parcial_2/Categoria.java
+++
@@ -1,7 +0,0 @@
-
-package santostapia_parcial_2;
-
-public enum Categoria {
-     CIENTIFICA, TRANSPORTE, MILITAR
-}
-
